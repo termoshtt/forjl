d56013405a2917444fe036fbfdc87c6d6a6e1296
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-05-28 17:00:46 +0900

    jl_alloc_array_1d

--- julia.f90
+++ julia.f90
@@ -4,6 +4,12 @@ module julia
 
   type, bind(C) :: jl_array_t
     type(c_ptr) :: data_
+    integer(c_size_t)  :: length
+    integer(c_int16_t) :: flags   ! FIXME Define binding to jl_array_flags_t bitfield
+    integer(c_int16_t) :: elsize
+    integer(c_int32_t) :: offset
+    integer(c_size_t)  :: nrows
+    integer(c_size_t)  :: maxsize ! FIXME This cannot represents 2d or higher dim cases
   endtype
 
   interface
@@ -19,6 +25,14 @@ module julia
       use iso_c_binding, only: c_int
       integer(kind=c_int) st
     end subroutine
+
+    function jl_alloc_array_1d(atype, size) result(array) bind(c)
+      use iso_c_binding
+      import jl_array_t
+      type(c_ptr)               :: atype
+      integer(kind=c_size_t)    :: size
+      type(jl_array_t), pointer :: array
+    end
   end interface
 
 contains
